da12e5d0e5d650c8c8c990db4bcb0b74d2c49915
amrnator <amrsaleh129@gmail.com>
2019-04-10 19:41:57 -0400

    added test files

---
+++ test/test_cousin.py
@@ -0,0 +1,15 @@
+import sys
+import os.path
+
+import lark
+from nose.tools import assert_equals
+
+from gedcom.validate.gay_marriage import validate_check_if_gay
+from gedcom.transformer import GedcomTransformer
+from gedcom.models import *
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+def test_cousin_marriage():
+
+    assert True;
